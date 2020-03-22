170b51dd8f6b1a23af84d16599a604158663ceec
Matthias Maag <maag.matthias@outlook.com>
2020-03-22 22:37:29 +0100

    add a second activity

--- app/src/main/java/de/mmsoftware/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/de/mmsoftware/myfirstandroidapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package de.mmsoftware.myfirstandroidapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,5 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply{
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
